7c9eab573d87e7518f72d10233f27de5817ac079
Sourav Chatterjee <sromic@gmail.com>
2018-09-09 19:15:47 +0530

    - Initial upload

---
+++ Assets/Scripts/WaypointBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointBehavior : MonoBehaviour 
+{
+    // Public variables
+    public Color targetedColor;
+    public Color normalColor;
+
+    // Private variables
+    Material mat;
+
+	private void Start()
+	{
+        mat = GetComponent<Renderer>().material;
+        mat.color = normalColor;
+	}
+
+	public void OrbTargeted()
+    {
+        mat.color = targetedColor;
+    }
+
+    public void OrbReset()
+    {
+        mat.color = normalColor;
+    }
+}
